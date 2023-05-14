840ac87be2b290f3f8b6346e9eb117c2dd146515
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2023-05-14 18:38:47 +0300

    Genel Proje Mimarisi

---
+++ Prosperst.Domain/Enumerations/CustomerStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace Prosperst.Domain.Enumerations
+{
+    public enum CustomerStatus
+    {
+        [Description("Kimlik Doğrulama Bekleniyor")]
+        WAITING_IDENTITY_VERIFICATION = 1,
+        [Description("Kimlik Doğrulandı")]
+        IDENTITY_VERIFICATED = 2,
+        [Description("Kimlik Doğrulanamadı")]
+        IDENTITY_NOT_VERIFICATED = 3
+    }
+}
